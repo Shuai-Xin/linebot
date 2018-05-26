ae682d65771e47ac8304c50876a67438146346cb
Shuai-Xin <xinver22@gmail.com>
2018-05-26 10:32:02 +0800

    3

---
+++ index.js
@@ -0,0 +1,8 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+  channelId:1582996232,
+  channelSecret:aea9c4bea0639a96ceb76d62302a285b,
+  channelAccessToken:Y4t0mzXQkaLGQKDhLQNoyt4WZojtsLdudyiYjTkYydYGJG7IzxPSeyMjwFBxrSs/8Klz2Ka2fRVanEA0AwrdklZYjecIue19KuBZoxYFh43UeWhjsHO2cvvOxM1J4YfI+T4I69osANQRPTrR/KMAdAdB04t89/1O/w1cDnyilFU=
+});
